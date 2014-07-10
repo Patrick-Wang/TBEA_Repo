a69d469da75b3a3a298a1623d86dea114dc48bac
FloydSun <sun.sunshine@163.com>
2014-07-10 17:11:20 +0800

    full function

--- TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/dataviewer/FuturesChartPage.java
+++ TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/dataviewer/FuturesChartPage.java
@@ -2,22 +2,52 @@ package com.dataviewer;
 
 import com.example.dataviewer.R;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
-public class FuturesChartPage extends AQueryFragment  {
+public class FuturesChartPage extends AQueryFragment implements
+		OnCheckedChangeListener {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater
 				.inflate(R.layout.futures_chart_page, container, false);
 		update(v);
+		((RadioGroup) aq.id(R.id.rg_tab_ac).getView())
+				.setOnCheckedChangeListener(this);
 
+		aq.id(R.id.detailbtn).clicked(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				FragmentTransaction ft = getActivity().getFragmentManager()
+						.beginTransaction();
+				ft.hide(FuturesChartPage.this);
+				ft.replace(R.id.host, new FuturesTablePage()).addToBackStack(
+						null);
+				ft.commit();
+			}
+		});
 
 		return v;
 	}
 
-	
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		switch (checkedId) {
+		case R.id.cu:
+			break;
+		case R.id.al:
+			break;
+		default:
+			break;
+		}
+	}
+
 }
--- TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/excel/ContentLinearLayout.java
+++ TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/excel/ContentLinearLayout.java
@@ -1,9 +1,6 @@
 package com.excel;
 
-import java.util.List;
-
 import android.content.Context;
-import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
@@ -32,30 +29,7 @@ public class ContentLinearLayout extends LinearLayout {
 		this.sheet = sheet;
 	}
 	
-	@Override
-	protected void onDraw(Canvas canvas) {
-		//super.onDraw(canvas);
-		
-//		List<Integer> rowHeight = sheet.getRowHeight();
-//		List<Integer> columWidth = sheet.getColumWidth();
-//		int width = sheet.getContentWidth();
-//		int height = sheet.getContentHeight();
-//		
-//		for (int i = columWidth.size() - 1; i >= 1; --i){
-//			canvas.drawLine(width - 1, 0, width, height, paint);
-//			width -= columWidth.get(i);
-//		}
-//		
-//		for (int j = rowHeight.size() - 1; j >= 1; --j){
-//			canvas.drawLine(0, height, width, height, paint);
-//			height -= rowHeight.get(j);
-//		}
-		
-		
 
-		
-	}
-//	
 	
 
 }
