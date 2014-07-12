16dc40ccef9ad8c79a6d58d5139d1fd864cbcf40
yida1702 <yida_extra@126.com>
2014-07-12 17:28:02 +0800

    echarts update

--- TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/dataviewer/FundsChartPage.java
+++ TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/dataviewer/FundsChartPage.java
@@ -21,108 +21,176 @@ import com.example.dataviewer.R;
 public class FundsChartPage extends AQueryFragment implements
 		OnCheckedChangeListener {
 
-	public WebView webView = null;
+	public WebView receiveable_Ratio_WebView = null;
+	public WebView daily_Payment_WebView = null;
+	public WebView monthly_Payment_WebView = null;
 
 	public Handler handler = new Handler();
 
 	public ProgressDialog dialog = null;
 
 	@Override
+	public void onDestroy() {
+		if (null != dialog) {
+			dialog.dismiss();
+		}
+		super.onDestroy();
+	}
+
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+
+		dialog = ProgressDialog.show(getActivity(), null, "数据加载中，请稍后...");
+
+		// if (aq == null) {
 		View v = inflater.inflate(R.layout.funds_chart_page, container, false);
 		update(v);
 		((RadioGroup) aq.id(R.id.rg_tab).getView())
 				.setOnCheckedChangeListener(this);
 
-		webView = (WebView) v.findViewById(R.id.receiveable_ratio_webview);
-		initView("file:///android_asset/receivable_ratio.html");
-		// refresh();
+		receiveable_Ratio_WebView = (WebView) v
+				.findViewById(R.id.receiveable_ratio_webview);
+		initView(receiveable_Ratio_WebView,
+				"file:///android_asset/receivable_ratio.html",
+				new WebViewClient() {
+					@Override
+					public void onPageFinished(WebView view, String url) {
+						refresh(R.id.receiveable_ratio_webview);
+					}
+				});
+
+		// } else {
+		// refresh(R.id.receiveable_ratio_webview);
+		// }
 
 		return v;
 	}
 
 	@Override
 	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		dialog.show();
 		switch (checkedId) {
 		case R.id.receivable_money:
 			aq.id(R.id.receivableratio).visibility(View.VISIBLE);
 			aq.id(R.id.daily_payment).visibility(View.GONE);
 			aq.id(R.id.monthly_payment).visibility(View.GONE);
 
-			webView = (WebView) aq.id(R.id.receiveable_ratio_webview).getView();
-			initView("file:///android_asset/receivable_ratio.html");
-			// refresh();
-
+			refresh(R.id.receiveable_ratio_webview);
 			break;
 		case R.id.day_signed:
 			aq.id(R.id.receivableratio).visibility(View.GONE);
 			aq.id(R.id.daily_payment).visibility(View.VISIBLE);
 			aq.id(R.id.monthly_payment).visibility(View.GONE);
 
-			webView = (WebView) aq.id(R.id.daily_payment_webview).getView();
-			initView("file:///android_asset/daily_payment.html");
-			// refresh();
-
+			if (null == daily_Payment_WebView) {
+				daily_Payment_WebView = (WebView) getActivity().findViewById(
+						R.id.daily_payment_webview);
+				initView(daily_Payment_WebView,
+						"file:///android_asset/daily_payment.html",
+						new WebViewClient() {
+							@Override
+							public void onPageFinished(WebView view, String url) {
+								refresh(R.id.daily_payment_webview);
+							}
+						});
+			} else {
+				refresh(R.id.daily_payment_webview);
+			}
 			break;
 		case R.id.month_sigend:
 			aq.id(R.id.receivableratio).visibility(View.GONE);
 			aq.id(R.id.daily_payment).visibility(View.GONE);
 			aq.id(R.id.monthly_payment).visibility(View.VISIBLE);
 
-			webView = (WebView) aq.id(R.id.monthly_payment_webview).getView();
-			initView("file:///android_asset/monthly_payment.html");
-			// refresh();
-
+			if (null == monthly_Payment_WebView) {
+				monthly_Payment_WebView = (WebView) getActivity().findViewById(
+						R.id.monthly_payment_webview);
+				initView(monthly_Payment_WebView,
+						"file:///android_asset/monthly_payment.html",
+						new WebViewClient() {
+							@Override
+							public void onPageFinished(WebView view, String url) {
+								refresh(R.id.monthly_payment_webview);
+							}
+						});
+			} else {
+				refresh(R.id.monthly_payment_webview);
+			}
 			break;
 		default:
 			break;
 		}
 	}
 
-	public void initView(/* WebView webView, */String url) {
+	public void initView(WebView webView, String url,
+			WebViewClient webViewClient) {
 		webView.getSettings().setJavaScriptEnabled(true);
 		webView.getSettings().setAllowFileAccess(true);
-		CookieManager.getInstance().setAcceptCookie(true);
 		webView.getSettings().setNeedInitialFocus(false);
-		webView.addJavascriptInterface(this, "SurveyUtil");
-		// webView.setWebViewClient(new WebViewClient() {
-		// @Override
-		// public void onPageFinished(WebView view, String url) {
-		// List<String> values = new ArrayList<String>();
-		// for (int i = 1; i <= 7; i++) {
-		// values.add(String.valueOf(i));
-		// }
-		// webView.loadUrl("javascript:refreshView(" + values + ");");
-		// }
-		// });
-		dialog = ProgressDialog.show(getActivity(),null,"页面加载中，请稍后..");
-		webView.setWebViewClient(new WebViewClient() {
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				dialog.dismiss();
-			}
-		});
-		// webView.setWebChromeClient(new WebChromeClientSelf(getActivity()));
-		// webView.setBackgroundColor(getResources().getColor(
-		// android.R.color.transparent));
+		webView
+				.addJavascriptInterface(this, "FundsChartPage");
+		webView.setBackgroundColor(getResources().getColor(
+				android.R.color.transparent));
+		webView.setWebViewClient(webViewClient);
 		webView.loadUrl(url);
 	}
 
-	public void refresh() {
-		handler.post(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
+	public void refresh(int id) {
+		switch (id) {
+		case R.id.receiveable_ratio_webview:
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+					List<String> values = new ArrayList<String>();
+					for (int i = 1; i <= 7; i++) {
+						values.add(String.valueOf(i));
+					}
+					receiveable_Ratio_WebView.loadUrl("javascript:refreshView("
+							+ values + ");");
 				}
-				List<String> values = new ArrayList<String>();
-				for (int i = 1; i <= 7; i++) {
-					values.add(String.valueOf(i));
+			});
+			break;
+		case R.id.daily_payment_webview:
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+					List<String> values = new ArrayList<String>();
+					for (int i = 1; i <= 7; i++) {
+						values.add(String.valueOf(i));
+					}
+					daily_Payment_WebView.loadUrl("javascript:refreshView("
+							+ values + ");");
 				}
-				webView.loadUrl("javascript:refreshView(" + values + ");");
-			}
-		});
+			});
+
+			break;
+		case R.id.monthly_payment_webview:
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+//					try {
+//						Thread.sleep(3000);
+//					} catch (InterruptedException e) {
+//						// TODO: handle exception
+//					}
+					List<String> values = new ArrayList<String>();
+					for (int i = 1; i <= 7; i++) {
+						values.add(String.valueOf(i));
+					}
+					monthly_Payment_WebView.loadUrl("javascript:refreshView("
+							+ values + ");");
+				}
+			});
+
+			break;
+		default:
+			dialog.hide();
+			break;
+		}
+	}
+
+	public void afterRefresh() {
+		dialog.dismiss();
 	}
 }
--- TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/dataviewer/FuturesChartPage.java
+++ TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/dataviewer/FuturesChartPage.java
@@ -9,7 +9,6 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.CookieManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -17,73 +16,71 @@ import com.example.dataviewer.R;
 
 public class FuturesChartPage extends AQueryFragment {
 
-	public WebView webView = null;
+	public WebView profit_Lost_Copper_WebView = null;
 
 	public Handler handler = new Handler();
-	
+
 	public ProgressDialog dialog = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View v = inflater
-				.inflate(R.layout.futures_chart_page, container, false);
-		update(v);
+		dialog = ProgressDialog.show(getActivity(), null, "数据加载中，请稍后...");
 
-		webView = (WebView) v.findViewById(R.id.profit_lost_webview);
-		initView("file:///android_asset/profit_lost_copper.html");
-		 refresh();
+//		if (aq == null) {
+			View v = inflater.inflate(R.layout.futures_chart_page, container,
+					false);
+			update(v);
+			profit_Lost_Copper_WebView = (WebView) v
+					.findViewById(R.id.profit_lost_webview);
+			initView("Profit_Lost_Copper",
+					"file:///android_asset/profit_lost_copper.html");
+//		} else {
+//			refresh();
+//		}
 
-		return v;
+		return aq.getView();
 	}
 
-	public void initView(String url) {
-		webView.getSettings().setJavaScriptEnabled(true);
-		webView.getSettings().setAllowFileAccess(true);
-		CookieManager.getInstance().setAcceptCookie(true);
-		webView.getSettings().setNeedInitialFocus(false);
-		webView.addJavascriptInterface(this, "SurveyUtil1");
-//		webView.setWebViewClient(new WebViewClient());
-		dialog = ProgressDialog.show(getActivity(),null,"页面加载中，请稍后..");
-		webView.setWebViewClient(new WebViewClient() {
+	public void initView(String jsInterfaceName, String url) {
+		profit_Lost_Copper_WebView.getSettings().setJavaScriptEnabled(true);
+		profit_Lost_Copper_WebView.getSettings().setAllowFileAccess(true);
+		profit_Lost_Copper_WebView.getSettings().setNeedInitialFocus(false);
+		profit_Lost_Copper_WebView
+				.addJavascriptInterface(this, jsInterfaceName);
+//		profit_Lost_Copper_WebView.setBackgroundColor(getResources().getColor(
+//				android.R.color.transparent));
+		profit_Lost_Copper_WebView.setWebViewClient(new WebViewClient() {
 			@Override
 			public void onPageFinished(WebView view, String url) {
-				dialog.dismiss();
-				// refresh();
+				refresh();
 			}
 		});
 
-//		refresh();
-//		webView.setWebChromeClient(new WebChromeClient() {
-//			@Override
-//			public void onProgressChanged(WebView view, int newProgress) {
-//				if (100 == newProgress) {
-//					refresh();
-//				}
-//			}
-//		});
-
-		// webView.setWebChromeClient(new WebChromeClient());
-		// webView.setBackgroundColor(getResources().getColor(
-		// android.R.color.transparent));
-		webView.loadUrl(url);
+		profit_Lost_Copper_WebView.loadUrl(url);
 	}
 
 	public void refresh() {
 		handler.post(new Runnable() {
 			@Override
 			public void run() {
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-				}
+//				try {
+//					Thread.sleep(3000);
+//				} catch (InterruptedException e) {
+//					// TODO: handle exception
+//				}
 				List<String> values = new ArrayList<String>();
 				for (int i = 1; i <= 7; i++) {
 					values.add(String.valueOf(i));
 				}
-				webView.loadUrl("javascript:refreshView(" + values + ");");
+				profit_Lost_Copper_WebView.loadUrl("javascript:refreshView("
+						+ values + ");");
 			}
 		});
+	}
+
+	public void afterRefresh() {
+		dialog.dismiss();
 	}
 
 }
