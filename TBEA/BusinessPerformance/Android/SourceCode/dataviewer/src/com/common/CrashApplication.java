769b03358144a581bfcc6b204edbd02d68002751
yida1702 <yida_extra@126.com>
2014-07-16 10:41:38 +0800

    add log by Xu yida

---
+++ TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/common/CrashApplication.java
@@ -0,0 +1,12 @@
+package com.common;
+
+import android.app.Application;
+
+public class CrashApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		CrashHandler crashHandler = CrashHandler.getInstance();
+		crashHandler.init(getApplicationContext());
+	}
+}
