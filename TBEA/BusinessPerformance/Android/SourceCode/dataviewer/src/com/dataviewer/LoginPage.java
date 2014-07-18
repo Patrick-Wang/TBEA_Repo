d165c9b4fa45fde35a34c837e2eacc2e2be686b9
FloydSun <sun.sunshine@163.com>
2014-07-18 13:05:25 +0800

    Changed by sunfuda loginpage

--- TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/dataviewer/LoginPage.java
+++ TBEA/BusinessPerformance/Android/SourceCode/dataviewer/src/com/dataviewer/LoginPage.java
@@ -1,6 +1,13 @@
 package com.dataviewer;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
 import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
 import com.tbea.dataviewer.R;
 
 import android.app.FragmentTransaction;
@@ -19,13 +26,8 @@ public class LoginPage extends AQueryFragment {
 
 			@Override
 			public void onClick(View v) {
-				if (auth(aq.id(R.id.usrn).getText().toString(), aq.id(R.id.psw)
-						.getText().toString())) {
-					FragmentTransaction ft = getActivity().getFragmentManager()
-							.beginTransaction();
-					ft.replace(R.id.host, new HomePage());
-					ft.commit();
-				}
+				auth(aq.id(R.id.usrn).getText().toString(), aq.id(R.id.psw)
+						.getText().toString());
 			}
 		});
 	}
@@ -36,7 +38,76 @@ public class LoginPage extends AQueryFragment {
 		return inflater.inflate(R.layout.loginpage, container, false);
 	}
 
-	private boolean auth(String usn, String psw) {
-		return true;
+	private void auth(String usn, String psw) {
+		String url = "http://218.84.134.160:8081/mobile/loginServlet";
+		try {
+
+			Map<String, String> map = new HashMap<String, String>();
+
+			map.put("username", "admin");
+			map.put("password", "123");
+			long id1 = Thread.currentThread().getId();
+			mAq.ajax(url, map, JSONObject.class,
+					new AjaxCallback<JSONObject>() {
+
+						@Override
+						public void callback(String url, JSONObject json,
+								AjaxStatus status) {
+							long id = Thread.currentThread().getId();
+							if (json != null) {
+								// successful ajax call, show status code and
+								// json content
+								FragmentTransaction ft = getActivity()
+										.getFragmentManager()
+										.beginTransaction();
+								ft.replace(R.id.host, new HomePage());
+								ft.commit();
+							} else {
+								// ajax error, show error code
+								// Toast.makeText(aq.getContext(), "Error:" +
+								// status.getCode(), Toast.LENGTH_LONG).show();
+							}
+						}
+
+					});
+
+			// AsyncHttpClient client = new AsyncHttpClient();
+			// RequestParams param = new RequestParams();
+			// param.add("username", "admin");
+			// param.add("password", "123");
+			// client.post(url, param, new TextHttpResponseHandler() {
+			//
+			// @Override
+			// public void onFailure(int arg0, Header[] arg1, String arg2,
+			// Throwable arg3) {
+			// Toast.makeText(getActivity(), "网络异常",
+			// Toast.LENGTH_SHORT).show();
+			// }
+			//
+			// @SuppressWarnings("unused")
+			// @Override
+			// public void onSuccess(int arg0, Header[] arg1, String arg2) {
+			//
+			// try {
+			// JSONObject json = new JSONObject(arg2);
+			// int k = 0;
+			// ++k;
+			// } catch (JSONException e) {
+			// // TODO Auto-generated catch block
+			// e.printStackTrace();
+			// }
+			//
+			// FragmentTransaction ft = getActivity().getFragmentManager()
+			// .beginTransaction();
+			// ft.replace(R.id.host, new HomePage());
+			// ft.commit();
+			//
+			// }
+			//
+			// });
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
