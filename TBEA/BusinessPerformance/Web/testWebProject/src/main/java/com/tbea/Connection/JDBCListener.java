cc8463fdeb606f4ca4f5380338fc837f7c43162d
FloydSun <sun.sunshine@163.com>
2014-11-18 16:42:58 +0800

    删除 JDBC 相关控制

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/Connection/JDBCListener.java
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/Connection/JDBCListener.java
@@ -13,13 +13,13 @@ public class JDBCListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
-		Thread trd = new Thread(new Runnable() {
-			 public void run(){
-				 DBConnectionManager.getInstance("mobileSys");
-				 System.out.println("JDBC connection finished!");
-			 }
-		});
-		trd.start();
+//		Thread trd = new Thread(new Runnable() {
+//			 public void run(){
+//				 DBConnectionManager.getInstance("mobileSys");
+//				 System.out.println("JDBC connection finished!");
+//			 }
+//		});
+//		trd.start();
 	}
 
 }
