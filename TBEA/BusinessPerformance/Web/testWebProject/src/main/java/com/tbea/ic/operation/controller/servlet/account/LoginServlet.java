bce24e0a4ae755a7b2b6e4922339339d4f272309
FloydSun <sun.sunshine@163.com>
2015-01-29 12:20:51 +0800

    增加login中权限控制

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/controller/servlet/account/LoginServlet.java
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/controller/servlet/account/LoginServlet.java
@@ -1,19 +1,33 @@
 package com.tbea.ic.operation.controller.servlet.account;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.tbea.ic.operation.model.entity.User;
+import com.tbea.ic.operation.service.approve.ApproveService;
+import com.tbea.ic.operation.service.entry.EntryService;
+
 @Controller
 @RequestMapping(value = "Login")
 public class LoginServlet {
 
 	private String view = "index";
+	
+	@Autowired
+	private EntryService service;
+	
+	@Autowired
+	private ApproveService approveService;
 
 	@RequestMapping(value = "login.do", method = RequestMethod.POST)
 	public ModelAndView importBLHT(HttpServletRequest request,
@@ -22,6 +36,16 @@ public class LoginServlet {
 			@RequestParam(value = "j_password") String j_password) {
 		System.out.println("j_username : " + j_username);
 		System.out.println("j_password : " + j_password);
-		return new ModelAndView(view);
+		Map<String, Object> map = new HashMap<String, Object>();
+		User usr = new User();
+		boolean hasPermission = service.hasEntryPlanPermission(usr);
+		map.put("entryPlan", true);
+		hasPermission = service.hasEntryPredictPermission(usr);
+		map.put("entryPredict", true);
+		hasPermission = approveService.hasApprovePlanPermission(usr);
+		map.put("approvePlan", true);
+		hasPermission = approveService.hasApprovePredictPermission(usr);
+		map.put("approvePredict", true);
+		return new ModelAndView(view, map);
 	}
 }
