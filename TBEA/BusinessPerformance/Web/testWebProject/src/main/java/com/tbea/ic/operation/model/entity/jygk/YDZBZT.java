4fedf83096401f7c429306da2bd9a6bc0874a8b8
yida <yida_extra@126.com>
2015-02-09 11:22:12 +0800

    月度指标状态

---
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/model/entity/jygk/YDZBZT.java
@@ -0,0 +1,77 @@
+package com.tbea.ic.operation.model.entity.jygk;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import cn.com.tbea.template.model.entity.AbstractReadWriteEntity;
+
+@Entity
+@Table(name = "jygk_ydzbzt")
+public class YDZBZT extends AbstractReadWriteEntity implements Serializable {
+
+	private static final long serialVersionUID = 1440655104212069401L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	public int getId() {
+		return super.getId();
+	}
+
+	private DWXX dwxx;
+
+	private Integer nf;
+
+	private Integer yf;
+
+	private Integer zt;
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "dwid")
+	public DWXX getDwxx() {
+		return dwxx;
+	}
+
+	/**
+	 * @param dwxx
+	 *            the dwxx to set
+	 */
+	public void setDwxx(DWXX dwxx) {
+		this.dwxx = dwxx;
+	}
+
+	public Integer getNf() {
+		return nf;
+	}
+
+	public void setNf(Integer nf) {
+		this.nf = nf;
+	}
+
+	public Integer getYf() {
+		return yf;
+	}
+
+	public void setYf(Integer yf) {
+		this.yf = yf;
+	}
+
+	public Integer getZt() {
+		return zt;
+	}
+
+	public void setZt(Integer zt) {
+		this.zt = zt;
+	}
+
+}
