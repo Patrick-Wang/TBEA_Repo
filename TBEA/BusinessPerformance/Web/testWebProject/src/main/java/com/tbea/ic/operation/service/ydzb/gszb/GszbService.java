69c5a64c48431fd2bd8a3d4cdd13bd7be92597a4
FloydSun <sun.sunshine@163.com>
2015-02-13 16:40:57 +0800

    增加公司指标service

---
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/service/ydzb/gszb/GszbService.java
@@ -0,0 +1,8 @@
+package com.tbea.ic.operation.service.ydzb.gszb;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface GszbService {
+	public List<String[]> getGsztzb(Date date);
+}
---
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/service/ydzb/gszb/GszbServiceImpl.java
@@ -0,0 +1,14 @@
+package com.tbea.ic.operation.service.ydzb.gszb;
+
+import java.sql.Date;
+import java.util.List;
+
+public class GszbServiceImpl implements GszbService{
+
+	@Override
+	public List<String[]> getGsztzb(Date date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
