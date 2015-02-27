156319b40b7015fdaf6741696002be14d9b0ee52
FloydSun <sun.sunshine@163.com>
2015-02-27 10:12:49 +0800

    CompositeConfigurator 改名为 ZtzbCompositeConfigurator

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/service/ydzb/gszb/pipe/configurator/ZtzbCompositeConfigurator.java
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/service/ydzb/gszb/pipe/configurator/ZtzbCompositeConfigurator.java
@@ -14,6 +14,7 @@ import com.tbea.ic.operation.service.ydzb.gszb.pipe.filter.SpecialPipeFilter;
 import com.tbea.ic.operation.service.ydzb.gszb.pipe.filter.ZzlPipeFilter;
 import com.tbea.ic.operation.service.ydzb.gszb.pipe.filter.WclPipeFilter;
 
+//总体指标
 public class ZtzbCompositeConfigurator implements IPipeConfigurator {
 
 	private IAccumulator acc;
