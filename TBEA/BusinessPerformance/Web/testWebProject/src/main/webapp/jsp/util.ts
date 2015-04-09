6564454b56b4d2b31c1fa7c87211fa9eb27ebc96
FloydSun <sun.sunshine@163.com>
2015-04-09 16:01:47 +0800

    去掉千位符

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/webapp/jsp/util.ts
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/webapp/jsp/util.ts
@@ -276,43 +276,44 @@ module Util {
         if (val === "--" || val === "") {
             return val;
         }
-        val = parseFloat(val).toFixed(0) + "";
-        var dot: number = val.lastIndexOf('.');
-        var intPart: string = "";
-        var parts: string[] = [];
-        var positive: boolean = (val.charAt(0) != '-');
-        if (dot > 0) {
-            if (positive) {
-                intPart = val.substring(0, dot);
-            } else {
-                intPart = val.substring(1, dot);
-            }
-            parts.push(val.substring(dot));
-        }
-        else {
-            if (positive) {
-                intPart = val;
-            } else {
-                intPart = val.substring(1);
-            }
-        }
-
-        var leftLength: number = intPart.length;
-
-        while (leftLength > 3) {
-            parts.push("," + intPart.substring(leftLength - 3, leftLength));
-            leftLength -= 3;
-        }
-
-        parts.push(intPart.substring(0, leftLength));
-
-        if (!positive) {
-            parts.push("-");
-        }
-
-        parts = parts.reverse();
-
-        return parts.join("");
+        return parseFloat(val).toFixed(0) + "";
+//        val = parseFloat(val).toFixed(0) + "";
+//        var dot: number = val.lastIndexOf('.');
+//        var intPart: string = "";
+//        var parts: string[] = [];
+//        var positive: boolean = (val.charAt(0) != '-');
+//        if (dot > 0) {
+//            if (positive) {
+//                intPart = val.substring(0, dot);
+//            } else {
+//                intPart = val.substring(1, dot);
+//            }
+//            parts.push(val.substring(dot));
+//        }
+//        else {
+//            if (positive) {
+//                intPart = val;
+//            } else {
+//                intPart = val.substring(1);
+//            }
+//        }
+//
+//        var leftLength: number = intPart.length;
+//
+//        while (leftLength > 3) {
+//            parts.push("," + intPart.substring(leftLength - 3, leftLength));
+//            leftLength -= 3;
+//        }
+//
+//        parts.push(intPart.substring(0, leftLength));
+//
+//        if (!positive) {
+//            parts.push("-");
+//        }
+//
+//        parts = parts.reverse();
+//
+//        return parts.join("");
     }
 
     
