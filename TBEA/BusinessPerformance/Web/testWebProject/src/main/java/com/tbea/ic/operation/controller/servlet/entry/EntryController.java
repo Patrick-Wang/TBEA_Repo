7b39847302fdd09d5833b30e14c987f588efcca7
FloydSun <sun.sunshine@163.com>
2015-04-22 10:19:24 +0800

    add isJydw tag

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/controller/servlet/entry/EntryController.java
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/controller/servlet/entry/EntryController.java
@@ -127,7 +127,8 @@ public class EntryController {
 		Company zhgs = org.getCompany(CompanyType.ZHGS);
 		String result = "{\"status\":" + JSONArray.fromObject(approved).toString() +
 						", \"values\":" + zb +
-						", \"isJydw\":" + ((4 == company.level() && !zhgs.contains(company)) || comp == CompanyType.ZHGS) + "}";
+						", \"isJydw\":" + ((4 == company.level() && !zhgs.contains(company)) || comp == CompanyType.ZHGS) + "}"; 
+		
 		return result.getBytes("utf-8");
 	}
 	
