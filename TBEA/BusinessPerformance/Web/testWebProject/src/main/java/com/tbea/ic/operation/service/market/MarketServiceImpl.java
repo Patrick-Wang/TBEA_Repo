325e1acec224803b1caa9fd36298edeebcc9a566
FloydSun <sun.sunshine@163.com>
2015-08-31 16:30:54 +0800

    修改乱码

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/service/market/MarketServiceImpl.java
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/service/market/MarketServiceImpl.java
@@ -38,7 +38,7 @@ public class MarketServiceImpl implements MarketService {
 	private MktSignContractDao signContractDao;
 	
 	private final String ERROR_OK = "OK";
-	private final String ERROR_COUNT_NOT_MATCH = "鏂囨。涓嶅尮閰�(鍒楁暟涓嶅尮閰�)";
+	private final String ERROR_COUNT_NOT_MATCH = "文档不匹配(列数不匹配)";
 	
 	
 	interface OnUpdateMktObjectListener{
