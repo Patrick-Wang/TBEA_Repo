7e75b1af8db60433972395c08664a6579d761579
FloydSun <sun.sunshine@163.com>
2015-09-01 10:21:26 +0800

    update

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/service/market/MarketServiceImpl.java
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/service/market/MarketServiceImpl.java
@@ -96,8 +96,8 @@ public class MarketServiceImpl implements MarketService {
 	
 	String subZeroAndDot(String s){  
         if(s.indexOf(".") > 0){  
-            s = s.replaceAll("0+?$", "");//去掉多余的0  
-            s = s.replaceAll("[.]$", "");//如最后一位是.则去掉  
+            s = s.replaceAll("0+?$", "");
+            s = s.replaceAll("[.]$", "");
         }  
         return s;  
     }  
