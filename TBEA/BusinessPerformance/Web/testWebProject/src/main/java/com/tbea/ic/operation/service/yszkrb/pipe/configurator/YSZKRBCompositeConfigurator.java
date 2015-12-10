1d76079db26f2fde4dac2e6c9c0a49f73a05d4a5
EdwardAn <yy_hm1016@163.com>
2015-12-10 17:45:49 +0800

    修改Bug

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/service/yszkrb/pipe/configurator/YSZKRBCompositeConfigurator.java
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/service/yszkrb/pipe/configurator/YSZKRBCompositeConfigurator.java
@@ -29,23 +29,22 @@ AbstractCompositeConfigurator {
 	protected void onConfiguring(IPipe pipe, IAccumulator acc, List<Integer> zbs,
 			int startRow, int step, List<Company> subComps, WclPipeFilter wclFilter, ZzlPipeFilter tbzzFilter) {
 
-		List<Company> allCompanies = pipe.getCompanies();
 		SumPipeFilter sumFilter = new SumPipeFilter();
 		sumFilter.includeRow(startRow, step);
 		// 集团下达月度资金回笼指标
 		pipe.addFilter(new AccPipeFilter(acc, 0, startRow, step)
-					.includeCompanies(allCompanies))
+					.includeCompanies(subComps))
 
 		// 各单位自行制定的回款计划
 			.addFilter(new AccPipeFilter(acc, 1, startRow, step)
-					.includeCompanies(allCompanies))
+					.includeCompanies(subComps))
 
 		//今日回款
 			.addFilter(new AccPipeFilter(acc, 2, startRow, step)
-					.includeCompanies(allCompanies))
+					.includeCompanies(subComps))
 		//月累计（截止今日）月度从1号累计到月末31号汇总总额（即：今日回款中1号-31号回款累计）
 			.addFilter(new AccPipeFilter(acc, 3, startRow, step)
-					.includeCompanies(allCompanies))
+					.includeCompanies(subComps))
 					
 		// 资金回笼指标完成    月度累计回款/集团下达的计划
 			.addFilter(wclFilter.add(4, 3, 0))
@@ -56,15 +55,15 @@ AbstractCompositeConfigurator {
 			
 		//已回款中可降应收的回款金额
 			.addFilter(new AccPipeFilter(acc, 6, startRow, step)
-					.includeCompanies(allCompanies))
+					.includeCompanies(subComps))
 
 		//确保办出
 			.addFilter(new AccPipeFilter(acc, 7, startRow, step)
-					.includeCompanies(allCompanies))
+					.includeCompanies(subComps))
 
 		//争取办出
 			.addFilter(new AccPipeFilter(acc, 8, startRow, step)
-					.includeCompanies(allCompanies))
+					.includeCompanies(subComps))
 		
 		//两者合计   等于：确保办出+争取办出
 			.addFilter(sumFilter.add(9, 8, 7))
@@ -77,7 +76,7 @@ AbstractCompositeConfigurator {
 			
 		//截止月底应收账款账面余额
 			.addFilter(new AccPipeFilter(acc, 12, startRow, step)
-					.includeCompanies(allCompanies))			
+					.includeCompanies(subComps))			
 		
 			.addFilter(sumFilter)
 			.addFilter(wclFilter);		
