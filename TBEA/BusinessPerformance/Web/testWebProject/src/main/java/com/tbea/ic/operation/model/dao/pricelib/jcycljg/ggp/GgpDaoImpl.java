cdbc2f4040c30e7bd8916c3429288b34c33727d3
FloydSun <sun.sunshine@163.com>
2016-03-30 15:07:12 +0800

    update

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/model/dao/pricelib/jcycljg/ggp/GgpDaoImpl.java
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/model/dao/pricelib/jcycljg/ggp/GgpDaoImpl.java
@@ -1,14 +1,15 @@
 package com.tbea.ic.operation.model.dao.pricelib.jcycljg.ggp;
 
 
-import com.tbea.ic.operation.model.entity.pricelib.jcycljg.GgpEntity;
-import com.speed.frame.model.dao.AbstractReadWriteDaoImpl;
-import com.tbea.ic.operation.model.dao.pricelib.jcycljg.ggp.GgpDao;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import cn.com.tbea.template.model.dao.AbstractReadWriteDaoImpl;
+
+import com.tbea.ic.operation.model.entity.pricelib.jcycljg.GgpEntity;
 
 
 
