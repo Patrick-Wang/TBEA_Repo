96c1fb83f1b33b3c5f292c88984707423035aca1
Patrick-Wang <wangx229@gmail.com>
2016-05-06 15:22:02 +0800

    update

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/controller/servlet/cwgbjyxxjl/ndjyxxjl/NdjyxxjlServlet.java
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/controller/servlet/cwgbjyxxjl/ndjyxxjl/NdjyxxjlServlet.java
@@ -3,6 +3,7 @@ package com.tbea.ic.operation.controller.servlet.cwgbjyxxjl.ndjyxxjl;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.sql.Date;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -16,11 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tbea.ic.operation.common.CompanySelection;
-import com.tbea.ic.operation.common.ErrorCode;
-import com.tbea.ic.operation.common.Util;
 import com.tbea.ic.operation.common.companys.Company;
 import com.tbea.ic.operation.common.companys.CompanyManager;
 import com.tbea.ic.operation.common.companys.CompanyType;
+import com.tbea.ic.operation.common.excel.CwgbjyxxjlSheetType;
+import com.tbea.ic.operation.common.excel.ExcelTemplate;
+import com.tbea.ic.operation.common.formatter.excel.FormatterClient;
+import com.tbea.ic.operation.common.formatter.excel.FormatterHandler;
+import com.tbea.ic.operation.common.formatter.excel.HeaderCenterFormatterHandler;
+import com.tbea.ic.operation.common.formatter.excel.MergeRegion;
+import com.tbea.ic.operation.common.formatter.excel.NumberFormatterHandler;
 import com.tbea.ic.operation.common.formatter.raw.RawEmptyHandler;
 import com.tbea.ic.operation.common.formatter.raw.RawFormatterClient;
 import com.tbea.ic.operation.common.formatter.raw.RawFormatterHandler;
@@ -59,59 +65,48 @@ public class NdjyxxjlServlet {
 	@RequestMapping(value = "entry/update.do")
 	public @ResponseBody byte[] updateNdjyxxjl(HttpServletRequest request,
 			HttpServletResponse response) throws UnsupportedEncodingException {
-		Date d = Date.valueOf(request.getParameter("date"));
-		CompanyType comp = CompanySelection.getCompany(request);
-		Company company = companyManager.getBMDBOrganization().getCompany(comp);
-		
-		List<List<String>> result = null; //ndjyxxjlService.getNdjyxxjlEntry(d, company);
-		
-		RawFormatterHandler handler = new RawNumberFormatterHandler(4, null, new Integer[]{3}).trimZero(true);
-		RawFormatterClient client = new RawFormatterClient(handler);
-		client.acceptNullAs("").format(result);
-		
-		return JSONArray.fromObject(result).toString().getBytes("utf-8");
+		return null;
 	}
 	
 	@RequestMapping(value = "entry/save.do")
 	public @ResponseBody byte[] saveNdjyxxjl(HttpServletRequest request,
 			HttpServletResponse response) throws UnsupportedEncodingException {
-		JSONArray data = JSONArray.fromObject(request.getParameter("data"));
-		Date d = Date.valueOf(request.getParameter("date"));
-		CompanyType comp = CompanySelection.getCompany(request);
-		Company company = companyManager.getBMDBOrganization().getCompany(comp);
-		
-		ErrorCode err = null;//ndjyxxjlService.saveNdjyxxjl(d, data, company);
-		return Util.response(err);
+		return null;
 	}
 	
 	@RequestMapping(value = "entry/submit.do")
 	public @ResponseBody byte[] submitNdjyxxjl(HttpServletRequest request,
 			HttpServletResponse response) throws UnsupportedEncodingException {
-		JSONArray data = JSONArray.fromObject(request.getParameter("data"));
-		Date d = Date.valueOf(request.getParameter("date"));
-		CompanyType comp = CompanySelection.getCompany(request);
-		Company company = companyManager.getBMDBOrganization().getCompany(comp);
+		return null;
+	}
+	
+	private CwgbjyxxjlSheetType getCwgbjyxxjlSheetType(Date d){
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
 		
-		ErrorCode err = null;//ndjyxxjlService.submitNdjyxxjl(d, data, company);
-		return Util.response(err);
+		Integer num = CwgbjyxxjlSheetType.ND_1.ordinal() + cal.get(Calendar.MONTH);
+		
+		CwgbjyxxjlSheetType sheetType = CwgbjyxxjlSheetType.values()[num];
+		
+		return sheetType;
 	}
 	
 	@RequestMapping(value = "export.do")
 	public void exportNdjyxxjl(HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
-//		Date d = Date.valueOf(request.getParameter("date"));
-//		CompanyType comp = CompanySelection.getCompany(request);
-//		Company company = companyManager.getBMDBOrganization().getCompany(comp);
-//		
-//		List<List<String>> result = ndjyxxjlService.getNdjyxxjl(d, company);
-//		ExcelTemplate template = ExcelTemplate.createCwgbjyxxjlTemplate(CwgbjyxxjlSheetType.NDJYXXJL);
-//	
-//		FormatterHandler handler = new HeaderCenterFormatterHandler(null, new Integer[]{0});
-//		handler.next(new NumberFormatterHandler(1));
-//		FormatterClient client = new FormatterClient(handler, 0, 2);
-//		client.addMergeRegion(new MergeRegion(0, 2, 1, result.size()));
-//		client.format(result, template);
-//	
-//		template.write(response, template.getSheetName() + ".xls");
+		Date d = Date.valueOf(request.getParameter("date"));
+		CompanyType comp = CompanySelection.getCompany(request);
+		Company company = companyManager.getBMDBOrganization().getCompany(comp);
+		
+		List<List<String>> result = jyxxjlService.getJyxxjlND(d, company);
+		ExcelTemplate template = ExcelTemplate.createCwgbjyxxjlTemplate(getCwgbjyxxjlSheetType(d));
+	
+		FormatterHandler handler = new HeaderCenterFormatterHandler(null, new Integer[]{0});
+		handler.next(new NumberFormatterHandler(1));
+		FormatterClient client = new FormatterClient(handler, 0, 2);
+		client.addMergeRegion(new MergeRegion(0, 2, 1, result.size()));
+		client.format(result, template);
+	
+		template.write(response, template.getSheetName() + "月.xls");
 	}
 }
