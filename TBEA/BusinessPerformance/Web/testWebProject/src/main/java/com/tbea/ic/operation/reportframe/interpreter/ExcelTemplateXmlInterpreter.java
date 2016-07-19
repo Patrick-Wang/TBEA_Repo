e0d9da42ce319ebd125cdc32b5d4af0d479b1427
FloydSun <sun.sunshine@163.com>
2016-07-19 15:16:10 +0800

    update

--- TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/reportframe/interpreter/ExcelTemplateXmlInterpreter.java
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/java/com/tbea/ic/operation/reportframe/interpreter/ExcelTemplateXmlInterpreter.java
@@ -5,19 +5,21 @@ import org.w3c.dom.Element;
 
 import com.tbea.ic.operation.common.excel.ExcelTemplate;
 import com.tbea.ic.operation.reportframe.component.AbstractXmlComponent;
+import com.tbea.ic.operation.reportframe.el.ELParser;
+import com.tbea.ic.operation.reportframe.util.XmlUtil;
 
 
 public class ExcelTemplateXmlInterpreter implements XmlInterpreter {
 
 	@Override
-	public boolean accept(AbstractXmlComponent component, Element e) {
+	public boolean accept(AbstractXmlComponent component, Element e) throws Exception {
 		
 		if (!Schema.isExcelTemplate(e)){
 			return false;
 		}
 
-
-		String sheetName = e.getAttribute("sheet");
+		
+		String sheetName = XmlUtil.getString(e.getAttribute("sheet"), new ELParser(component));
 		HSSFWorkbook workbook = new HSSFWorkbook();
 		workbook.createSheet(sheetName);
 
