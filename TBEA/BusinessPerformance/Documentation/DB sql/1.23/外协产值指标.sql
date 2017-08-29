7ba9d663ad8be83ecacb352cc6ef9b2eb1f13fde
EdwardAn <yy_hm1016@163.com>
2017-08-29 13:21:04 +0800

    update

---
+++ TBEA/BusinessPerformance/Documentation/DB sql/1.23/外协产值指标.sql
@@ -0,0 +1,28 @@
+﻿insert into dbo.jygk_dw_refer_zb_jhzb(  dwid,zbid) VALUES (1,353)
+insert into dbo.jygk_dw_refer_zb_jhzb (  dwid,zbid) VALUES (2,353)
+insert into dbo.jygk_dw_refer_zb_jhzb (  dwid,zbid) VALUES (3,353)
+insert into dbo.jygk_dw_refer_zb_jhzb (  dwid,zbid) VALUES (4,353)
+insert into dbo.jygk_dw_refer_zb_jhzb (  dwid,zbid) VALUES (5,353)
+insert into dbo.jygk_dw_refer_zb_jhzb (  dwid,zbid) VALUES (6,353)
+insert into dbo.jygk_dw_refer_zb_jhzb (  dwid,zbid) VALUES (9,353)
+insert into dbo.jygk_dw_refer_zb_jhzb (  dwid,zbid) VALUES (10,353)
+insert into dbo.jygk_dw_refer_zb_jhzb (  dwid,zbid) VALUES (13,353)
+
+
+insert into dbo.jygk_dw_refer_zb_sjzb(  dwid,zbid) VALUES (1,353)
+insert into dbo.jygk_dw_refer_zb_sjzb (  dwid,zbid) VALUES (2,353)
+insert into dbo.jygk_dw_refer_zb_sjzb (  dwid,zbid) VALUES (3,353)
+insert into dbo.jygk_dw_refer_zb_sjzb (  dwid,zbid) VALUES (4,353)
+insert into dbo.jygk_dw_refer_zb_sjzb (  dwid,zbid) VALUES (5,353)
+insert into dbo.jygk_dw_refer_zb_sjzb (  dwid,zbid) VALUES (6,353)
+insert into dbo.jygk_dw_refer_zb_sjzb (  dwid,zbid) VALUES (9,353)
+insert into dbo.jygk_dw_refer_zb_sjzb (  dwid,zbid) VALUES (10,353)
+insert into dbo.jygk_dw_refer_zb_sjzb (  dwid,zbid) VALUES (13,353)
+
+
+
+
+
+
+
+
