fe2d53f9b485649800844f42d2516fb19c03e117
FloydSun <sun.sunshine@163.com>
2017-09-08 11:06:17 +0800

    update

--- TBEA/BusinessPerformance/Documentation/DB sql/1.25/APP ID.sql
+++ TBEA/BusinessPerformance/Documentation/DB sql/1.25/APP ID.sql
@@ -1,2 +1,4 @@
 alter table jygk_account add appId varchar(100)
+
+insert into jygk_account (name, password, role, appId) values ('风控系统', '1234', 0, '03365721fe543f89032486fd9e90eb42')
 --substring(sys.fn_sqlvarbasetostr(HashBytes('MD5',convert(varchar(100), newid()))),3,32)
