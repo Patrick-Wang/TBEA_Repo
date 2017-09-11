5c316056fe4e0bf0ccf078e9e51ca84901ade9ad
FloydSun <sun.sunshine@163.com>
2017-09-11 17:08:10 +0800

    update

---
+++ TBEA/BusinessPerformance/Documentation/DB sql/1.25/印能到沈变数据库指标导入.sql
@@ -0,0 +1,51 @@
+insert into jygk_ydjhzb  (dwid, zbid, nf, yf, ydjhz, ydjhshzt) 
+select 1, 355, nf, yf, ydjhz, ydjhshzt from jygk_ydjhzb where dwid = 115 and zbid = 48
+
+insert into jygk_ydjhzb  (dwid, zbid, nf, yf, ydjhz, ydjhshzt) 
+select 1, 356, nf, yf, ydjhz, ydjhshzt from jygk_ydjhzb where dwid = 115 and zbid = 290
+
+insert into jygk_ydjhzb  (dwid, zbid, nf, yf, ydjhz, ydjhshzt) 
+select 1, 357, nf, yf, ydjhz, ydjhshzt from jygk_ydjhzb where dwid = 115 and zbid = 299
+
+
+
+insert into jygk_ndjhzb  (dwid, zbid, nf, ndjhz, ndjhshzt) 
+select 1, 355, nf, ndjhz, ndjhshzt from jygk_ndjhzb where dwid = 115 and zbid = 48
+
+insert into jygk_ndjhzb  (dwid, zbid, nf, ndjhz, ndjhshzt) 
+select 1, 356, nf, ndjhz, ndjhshzt from jygk_ndjhzb where dwid = 115 and zbid = 290
+
+insert into jygk_ndjhzb  (dwid, zbid, nf, ndjhz, ndjhshzt) 
+select 1, 357, nf, ndjhz, ndjhshzt from jygk_ndjhzb where dwid = 115 and zbid = 299
+
+
+insert into jygk_yj20zb  (dwid, zbid, nf, yf, yj20z, yj20shzt) 
+select 1, 355, nf,  yf,yj20z, yj20shzt from jygk_yj20zb where dwid = 115 and zbid = 48
+
+insert into jygk_yj20zb  (dwid, zbid, nf,  yf,yj20z, yj20shzt) 
+select 1, 356, nf,  yf,yj20z, yj20shzt from jygk_yj20zb where dwid = 115 and zbid = 290
+
+insert into jygk_yj20zb  (dwid, zbid, nf,  yf,yj20z, yj20shzt) 
+select 1, 357, nf,  yf,yj20z, yj20shzt from jygk_yj20zb where dwid = 115 and zbid = 299
+
+
+insert into jygk_yj28zb  (dwid, zbid, nf, yf, yj28z, yj28shzt) 
+select 1, 355, nf,  yf,yj28z, yj28shzt from jygk_yj28zb where dwid = 115 and zbid = 48
+
+insert into jygk_yj28zb  (dwid, zbid, nf,  yf,yj28z, yj28shzt) 
+select 1, 356, nf,  yf,yj28z, yj28shzt from jygk_yj28zb where dwid = 115 and zbid = 290
+
+insert into jygk_yj28zb  (dwid, zbid, nf,  yf,yj28z, yj28shzt) 
+select 1, 357, nf,  yf,yj28z, yj28shzt from jygk_yj28zb where dwid = 115 and zbid = 299
+
+
+insert into jygk_sjzb  (dwid, zbid, nf, yf, sjz, sjshzt) 
+select 1, 355, nf,  yf,sjz, sjshzt from jygk_sjzb where dwid = 115 and zbid = 48
+
+insert into jygk_sjzb  (dwid, zbid, nf,  yf,sjz, sjshzt) 
+select 1, 356, nf,  yf,sjz, sjshzt from jygk_sjzb where dwid = 115 and zbid = 290
+
+insert into jygk_sjzb  (dwid, zbid, nf,  yf,sjz, sjshzt) 
+select 1, 357, nf,  yf,sjz, sjshzt from jygk_sjzb where dwid = 115 and zbid = 299
+
+
