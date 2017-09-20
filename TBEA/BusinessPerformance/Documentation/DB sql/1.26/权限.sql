0e638df38327d6e75e90841161f3f96e12e21526
EdwardAn <yy_hm1016@163.com>
2017-09-20 17:38:14 +0800

    update

--- TBEA/BusinessPerformance/Documentation/DB sql/1.26/权限.sql
+++ TBEA/BusinessPerformance/Documentation/DB sql/1.26/权限.sql
@@ -1,2 +1,5 @@
 update auth_instruction set instruction = '人力信息查看' where id = 66
 update auth_instruction set instruction = '新产品信息查看' where id = 67
+update auth_instruction set instruction = '指标填报情况' where id = 6
+update auth_instruction set instruction = '能源周边市场情况录入' where id = 18
+update auth_instruction set instruction = '能源周边市场情况查看' where id = 19
