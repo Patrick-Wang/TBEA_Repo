82116ff6c4c75e041c11133947cad0de9ad96ddc
FloydSun <sun.sunshine@163.com>
2017-12-21 14:47:10 +0800

    update

---
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/webapp/jsp/ui2/pages/framework/templates/cwwh/entry.js
@@ -0,0 +1,73 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+///<reference path="../../route/route.ts"/>
+///<reference path="../../basic/basicdef.ts"/>
+///<reference path="../../../messageBox.ts"/>
+///<reference path="../../../components/dateSelectorProxy.ts"/>
+///<reference path="../singleDateReport/entry.ts"/>
+var framework;
+(function (framework) {
+    var templates;
+    (function (templates) {
+        var dateReport;
+        (function (dateReport) {
+            var Formula = JQTable.Formula;
+            var Cell = JQTable.Cell;
+            function createInstance() {
+                return new EntryView();
+            }
+            dateReport.createInstance = createInstance;
+            var EntryView = (function (_super) {
+                __extends(EntryView, _super);
+                function EntryView() {
+                    return _super !== null && _super.apply(this, arguments) || this;
+                }
+                EntryView.prototype.onInitialize = function (opt) {
+                    this.unitedSelector = new Util.UnitedSelector(opt.itemNodes, opt.itemId);
+                    if (opt.itemNodes.length == 1) {
+                        this.unitedSelector.hide();
+                        if ($("#" + opt.dtId).parent().hasClass("hidden")) {
+                            $("#grid-update").hide();
+                            $("#" + opt.dtId).hide();
+                        }
+                    }
+                    _super.prototype.onInitialize.call(this, opt);
+                };
+                EntryView.prototype.getParams = function (date) {
+                    return {
+                        date: this.getDate(date),
+                        item: this.unitedSelector.getDataNode(this.unitedSelector.getPath()).data.id
+                    };
+                };
+                EntryView.prototype.createJqassist = function () {
+                    var parent = $("#" + this.opt.host);
+                    var pagername = this.jqgridName() + "pager";
+                    parent.empty();
+                    parent.append("<table id='" + this.jqgridName() + "'><div id='" + pagername + "'></table>");
+                    this.mTableAssist = Util.createTable(this.jqgridName(), this.resp);
+                    var dst = new Cell(0, this.resp.dest);
+                    var srs = [new Cell(0, this.resp.f1), new Cell(0, this.resp.f2)];
+                    var form = new Formula(dst, srs, function (dest, srcs) {
+                        var f1 = srcs[0].getVal();
+                        var f2 = srcs[1].getVal();
+                        if (f1 != "" && f2 != "") {
+                            return f1 - f2;
+                        }
+                    });
+                    this.mTableAssist.addFormula(form);
+                    return this.mTableAssist;
+                };
+                return EntryView;
+            }(framework.templates.singleDateReport.EntryView));
+            dateReport.EntryView = EntryView;
+        })(dateReport = templates.dateReport || (templates.dateReport = {}));
+    })(templates = framework.templates || (framework.templates = {}));
+})(framework || (framework = {}));
---
+++ TBEA/BusinessPerformance/Web/testWebProject/src/main/webapp/jsp/ui2/pages/framework/templates/cwwh/entry.ts
@@ -0,0 +1,67 @@
+///<reference path="../../route/route.ts"/>
+///<reference path="../../basic/basicdef.ts"/>
+///<reference path="../../../messageBox.ts"/>
+///<reference path="../../../components/dateSelectorProxy.ts"/>
+///<reference path="../singleDateReport/entry.ts"/>
+module framework.templates.dateReport {
+    import router = framework.router;
+    import Node = JQTable.Node;
+    import BasicEndpoint = framework.basic.BasicEndpoint;
+    import FrameEvent = framework.basic.FrameEvent;
+    import Formula = JQTable.Formula;
+    import Cell = JQTable.Cell;
+
+    export function createInstance() : EntryView{
+        return new EntryView();
+    }
+
+    interface EntryOption extends framework.templates.singleDateReport.EntryOption{
+        itemNodes:Util.IDataNode[];
+        itemId:string;
+    }
+
+    export class EntryView extends framework.templates.singleDateReport.EntryView{
+
+        unitedSelector : Util.UnitedSelector;
+
+        onInitialize(opt:EntryOption):void{
+            this.unitedSelector = new Util.UnitedSelector(opt.itemNodes,opt.itemId);
+            if (opt.itemNodes.length == 1){
+                this.unitedSelector.hide();
+                if($("#" + opt.dtId).parent().hasClass("hidden")){
+                    $("#grid-update").hide();
+                    $("#" + opt.dtId).hide();
+                }
+            }
+            super.onInitialize(opt);
+        }
+
+        getParams(date:Util.Date):any{
+            return {
+                date: this.getDate(date),
+                item: this.unitedSelector.getDataNode(this.unitedSelector.getPath()).data.id
+            };
+        }
+
+        createJqassist():JQTable.JQGridAssistant{
+            var parent = $("#" + this.opt.host);
+            var pagername = this.jqgridName() + "pager";
+            parent.empty();
+            parent.append("<table id='"+ this.jqgridName() +"'><div id='" + pagername + "'></table>");
+            this.mTableAssist = Util.createTable(this.jqgridName(), this.resp);
+
+            let dst = new Cell(0, this.resp.dest);
+            let srs = [new Cell(0, this.resp.f1), new Cell(0, this.resp.f2)];
+            let form : Formula  = new Formula(dst, srs, (dest:Cell, srcs:Cell[])=>{
+                let f1 = srcs[0].getVal();
+                let f2 = srcs[1].getVal();
+                if (f1 != "" && f2 != ""){
+                    return f1 - f2;
+                }
+            });
+            this.mTableAssist.addFormula(form);
+
+            return this.mTableAssist;
+        }
+    }
+}
